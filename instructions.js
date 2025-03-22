e5ccb266f3d180286ef582cba400d0d52307d318
Martin T <mtitus575@gmail.com>
2025-03-22 14:47:18 +0000

    Completed Objecttves

--- instructions.js
+++ instructions.js
@@ -1,10 +1,3 @@
-let story = 'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a stunning 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
-
-let storyWords = story.split(' ');
-let unnecessaryWord = 'literally';
-let misspelledWord = 'beautifull';
-let badWord = 'freaking';
-
 /* Lesson intro:
     In this project, you’ll use what you know about iterating over arrays to gather information and improve the quality of a paragraph. */
 
@@ -12,7 +5,7 @@ let badWord = 'freaking';
     1. The 'story' variable holds the paragraph we’ll be editing. In order to edit the story, we turn it into an array on line 3.
         - The .split() method separates the story string by the space character (' ') and stores each word as an element of the array.
         - To see the array we’ll be working with throughout the lesson, log storyWords to the console. 
-            -After you’ve viewed the storyWords array, comment out the console.log() statement before moving to the next task.
+        -After you’ve viewed the storyWords array, comment out the console.log() statement before moving to the next task.
 
     2. For a better visual comparison of the original and edited stories, we want to view the edited 'storyWords' array as a string. 
         - To change the storyWords array back into a readable string, we can invoke the .join() method on storyWords.
@@ -44,7 +37,7 @@ let badWord = 'freaking';
     Replacing Words:
     7. Now that we’ve removed the unnecessary words, let’s take care of any misspelled words in the story. We can reassign storyWords to a new array of spell-checked words using the .map() method!
         - Reassign storyWords to equal the invocation of the .map() method on the storyWords array.
-        - Setword as a parameter of .map()‘s callback function.
+        - Set word as a parameter of .map()‘s callback function.
         - In the callback’s body, define a conditional statement to check if the word argument is equal to the misspelledWord variable. If it is, return the correct spelling of the string, “beautiful”. If not, return word.
         - Take a look at the story string in the console to see the correct spellings! You can see one instance of the spellchecked word in the first sentence of the story.
 
@@ -68,49 +61,5 @@ let badWord = 'freaking';
 
     */
 
-    /* Add code below: */
-    // console.log(storyWords);
-
-    let count = 0;
-    storyWords.forEach(word => {count++})
-
-    storyWords = storyWords.filter(word => {return word != unnecessaryWord});
-
-    storyWords = storyWords.map(word => {
-        if (word === misspelledWord) {
-            return 'beautiful'
-        } else {
-            return word;
-        }
-    });
-
-    const badWordIndex = storyWords.findIndex(word => {
-        if (word === badWord) {
-            return word
-        }
-    });
-    console.log(badWordIndex);
-    // console.log(storyWords[78]) // checked what the indexed word was before changing it below
-
-    storyWords[78] = 'really'
-    console.log(storyWords[78]) //confirms the word was changed
-
-    const lengthCheck = storyWords.every(word => {
-        if (word.length <= 10) {
-            return word;
-        }
-    });
-    console.log(lengthCheck);
-
-    const longWordIndex = storyWords.findIndex(word => {
-        if (word.length >= 10) {
-            return word
-        }
-    });
-    console.log(longWordIndex);
-    // console.log(storyWords[47]);
-
-
-    console.log(count);
-    console.log(storyWords.join(' ')); // turns the storyWords array into a string. The space in the quotes seperates each element for readability
+    // Add code below:
     
